508a361a92417b0e17df945cd4e26415069308fb
Pedro Chambino <pchambino@gmail.com>
2013-09-30 12:35:05 +0100

    Use xargs in setup.

--- setup.sh
+++ setup.sh
@@ -2,23 +2,17 @@
 
 cd $(dirname $0)
 
-for FILE in $(find . -path "./.*" -type f | cut -c2-)
-do
-  CMD="ln -sf \"$(pwd)${FILE}\" \"${HOME}${FILE}\""
-  echo $CMD && [ $DRYRUN ] || eval $CMD
-done
+find . -path "./.*" -type f -not -path "./.git/*" | cut -c2- | \
+  xargs -t -I % ln -sf "$(pwd)%" "$HOME%"
 
 case $(uname) in
 Darwin)
-  SUBLIMETEXT3="${HOME}/Library/Application Support/Sublime Text 3"
+  SUBLIMETEXT3="$HOME/Library/Application Support/Sublime Text 3"
 ;;
 *)
-  SUBLIMETEXT3="${HOME}/.config/sublime-text-3"
+  SUBLIMETEXT3="$HOME/.config/sublime-text-3"
 ;;
 esac
 
-for FILE in $(find . -path "./Sublime Text 3*" -type f | cut -c17-)
-do
-  CMD="ln -sf \"$(pwd)${FILE}\" \"${SUBLIMETEXT3}${FILE}\""
-  echo $CMD && [ $DRYRUN ] || eval $CMD
-done
+find . -path "./Sublime Text 3*" -type f | cut -c17- | \
+  xargs -t -I % ln -sf "$(pwd)/Sublime Text 3%" "${SUBLIMETEXT3}%"

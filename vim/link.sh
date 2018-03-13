82255419d812c4f6dbb21888f0b1abebcca59524
Pedro Chambino <pchambino@gmail.com>
2018-03-13 14:13:21 +0000

    [vim] Set indent folding for haml and yaml

--- vim/link.sh
+++ vim/link.sh
@@ -4,11 +4,13 @@ cd `dirname $0`
 . ../scripts/link_file.sh
 
 mkdir -p ~/.vim
+link_file `pwd`/after ~/.vim/after
 link_file `pwd`/autoload ~/.vim/autoload
 link_file `pwd`/ftplugin ~/.vim/ftplugin
 link_file `pwd`/vimrc ~/.vimrc
 
 mkdir -p ~/.config/nvim
+link_file `pwd`/after ~/.config/nvim/after
 link_file `pwd`/autoload ~/.config/nvim/autoload
 link_file `pwd`/ftplugin ~/.config/nvim/ftplugin
 link_file `pwd`/vimrc ~/.config/nvim/init.vim

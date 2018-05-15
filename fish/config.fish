ba27586216167d0a3887a2a5e64cf46344120bce
Pedro Chambino <pchambino@gmail.com>
2018-05-15 11:22:51 +0100

    [fish] Ensure key bindings is set
    It was causing some issues and this fixed it for some reason. 🤷

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,9 @@
 # clear greeting message
 set fish_greeting
 
+# ensure key bindings is set (?)
+set fish_key_bindings fish_default_key_bindings
+
 for FILE in $HOME/.config/fish/config.d/*.fish
   source "$FILE"
 end

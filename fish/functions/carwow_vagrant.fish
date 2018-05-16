d2787585aa60db565af0b57f78a3900c5c40743d
Pedro Chambino <pchambino@gmail.com>
2018-05-16 11:10:25 +0100

    [fish] improve carwow_vagrant

--- fish/functions/carwow_vagrant.fish
+++ fish/functions/carwow_vagrant.fish
@@ -1,11 +1,20 @@
 function carwow_vagrant
-  set -l vagrant_dir (basename (pwd))
+  set -l vagrant_dir
 
-  cd ~/vagrant
-
-  if not vagrant status | grep -q running;
-    vagrant up $argv
+  if pwd | grep -q /projects/carwow/
+    set vagrant_dir (pwd | sed '/.*\/projects\/carwow\//s///')
   end
 
-  vagrant ssh -c "CARWOW_VAGRANT_DIR=$vagrant_dir \$SHELL"
+  cd ~/vagrant
+  git pull --ff
+
+  if not vagrant status | grep -q running
+    vagrant up --provision
+  end
+
+  if test -n "$vagrant_dir"
+    vagrant ssh -c "CARWOW_VAGRANT_DIR=$vagrant_dir \$SHELL"
+  else
+    vagrant ssh
+  end
 end

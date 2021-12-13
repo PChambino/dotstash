4993fbe4b1953efdffcbbe017e14acb1558f5a4e
Pedro Chambino <pchambino@gmail.com>
2021-12-13 16:10:48 +0000

    [bootstrap] install fish v3

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,10 +6,12 @@ case $(uname) in
     Darwin)
         homebrew/install.sh
         python/install.sh
-	;;
+        ;;
     Linux)
+        sudo apt install -y software-properties-common
+        sudo apt-add-repository ppa:fish-shell/release-3
         sudo apt install -y fish tmux neovim
-	;;
+        ;;
 esac
 
 for SCRIPT in */link.sh; do $SCRIPT; done
